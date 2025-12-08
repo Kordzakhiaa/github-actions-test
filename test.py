647ce57a1c2a536e4725c18b82fd3afcc307e775
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2025-12-08 15:01:01 +0400

    test

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from main import simple_func
 
-r1 = simple_func(1)
-r2 = simple_func(2)
 
-assert r1 == True
-assert r2 == False
+def test_simple_func():
+    r1 = simple_func(1)
+    r2 = simple_func(2)
+
+    assert r1 == 2
+    assert r2 == 1
